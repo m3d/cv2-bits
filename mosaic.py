4e1ce73e32e440c83850e7e4179191604a9d8a9f
martind <martind@mapfactor.com>
2014-03-22 20:34:03 +0100

    interactive correction or original positions

--- mosaic.py
+++ mosaic.py
@@ -11,27 +11,49 @@ import numpy as np
 import csv
 import math
 
-DESIRED_SIZE = 900,500
+DESIRED_SIZE = 1200,800
 
 def mosaic( csvFile ):
   result = None
   reader = csv.reader( open(csvFile) )
   for row in reader:
-    print row
-    filename, x, y, degAngle = row
+    filename = row[0]
+    x, y, degAngle = [int(t) for t in row[1:]]
     img = cv2.imread( filename, cv2.CV_LOAD_IMAGE_COLOR )
-    a = math.radians(int(degAngle))
-    M = np.float32( [[math.cos(a), math.sin(a), x],
-                     [-math.sin(a),math.cos(a),y],
-                     [0,0,1]])
-    img = cv2.warpPerspective(img,M,(900,500))
-    if result != None:
-#      result = cv2.addWeighted(result,0.7,img,0.3,0)
-      result = cv2.add(result,img)
-    else:
-      result = img
-  cv2.imshow('result', result) 
-  cv2.waitKey()
+    while True:
+      a = math.radians(int(degAngle))
+      M = np.float32( [[math.cos(a), math.sin(a), x],
+                       [-math.sin(a),math.cos(a),y],
+                       [0,0,1]])
+      img2 = cv2.warpPerspective(img,M,DESIRED_SIZE)
+      tmp = result
+      if tmp != None:
+        tmp = cv2.add(result,img2)
+      else:
+        tmp = img2
+      cv2.imshow('result', tmp) 
+      key = cv2.waitKey()
+      if key == 2424832: # left
+        x -= 1
+      elif key == 2555904: # right
+        x += 1
+      elif key == 2490368: # up
+        y -= 1
+      elif key == 2621440: # down
+        y += 1
+#      elif key = 
+      elif key == 113: # q turn clockwise
+        degAngle += 1
+      elif key == 97: # a turn anticlockwise
+        degAngle -= 1
+      elif key == 13:
+        break
+      elif key == 27: # ESC
+        return None
+      else:
+        print key
+    print "%s,%d,%d,%d" % (filename,x,y,degAngle)
+    result = tmp
   return result
 
 if __name__ == "__main__":
@@ -39,5 +61,6 @@ if __name__ == "__main__":
     print __doc__
     sys.exit(1)
   img = mosaic( sys.argv[1] )
-  cv2.imwrite( sys.argv[2], img )
+  if img != None:
+    cv2.imwrite( sys.argv[2], img )
 
